6321aea21c98cf6000d2f2c343708c1ca889762b
Hung Nguyen <nthung2112@gmail.com>
2025-05-03 22:48:43 +0700

    Add lib

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import App from "./app";
+
+// Ace Editor imports
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-one_dark";
+import "ace-builds/src-noconflict/ext-language_tools";
+import "ace-builds/src-noconflict/ext-searchbox";
+import "ace-builds/src-noconflict/ext-error_marker";
+
+// Styles
+import "@fontsource/source-sans-pro";
+import "remixicon/fonts/remixicon.css";
+import "./assets/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
